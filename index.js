3ddc6ed89ed18049a5abd7c3fc76171b34a3c816
sabbirRashed <srsabbirrahman12@gmail.com>
2026-08-20 23:33:36 +0600

    feat: add all company fetch API

--- index.js
+++ index.js
@@ -24,8 +24,19 @@ const run = async () => {
 
         const db = client.db('TalentGate');
         const jobCollection = db.collection('jobs');
-        const companyCollection = db.collection('companies')
+        const companyCollection = db.collection('companies');
+        const userCollection = db.collection('user');
 
+        // User related API
+        app.get('/api/users', async (req, res) => {
+            const cursor = userCollection.find().skip(3);
+            const result = await cursor.toArray();
+            res.send(result);
+
+        })
+
+
+        // Job related API
         app.get("/api/jobs", async (req, res) => {
             const query = {};
 
@@ -36,30 +47,45 @@ const run = async () => {
                 query.status = req.query.status;
             }
 
-            const cursor = await jobCollection.find(query);
+            const cursor = jobCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
 
         app.post("/api/jobs", async (req, res) => {
-            const newJobs = req.body;
+            const jobs = req.body;
+            const newJobs = {
+                ...jobs,
+                createdAt: new Date(),
+            }
             const result = await jobCollection.insertOne(newJobs);
             res.send(result)
         })
 
         // company related api
-        app.get('/api/my/company', async(req, res)=>{
-            const query={};
+        app.get("/api/companies", async (req, res) => {
+            const cursor = companyCollection.find().skip(6);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-            if(req.query.recruiterId){
+        app.get('/api/my/company', async (req, res) => {
+            const query = {};
+
+            if (req.query.recruiterId) {
                 query.recruiterId = req.query.recruiterId;
             }
-            const result = companyCollection.findOne(query);
-            return result;
+            const result = await companyCollection.findOne(query);
+            console.log("company:", result);
+            res.send(result || {});
         })
 
         app.post('/api/companies', async (req, res) => {
-            const newCompany = req.body;
+            const company = req.body;
+            const newCompany = {
+                ...company,
+                createdAt: new Date(),
+            }
             const result = await companyCollection.insertOne(newCompany);
             res.send(result);
         })
